74d3e6e3b5bf7204d8e12b3b768189ef687ddca7
karlos-hr <dev.carloshrios@gmail.com>
2024-08-11 12:35:56 +0200

    feat: tailwind feat: header,footer,skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SkeletonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
