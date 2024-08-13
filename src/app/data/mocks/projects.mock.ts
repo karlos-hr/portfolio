54950dc8c5ea16bd8cdc19def8567195954bed71
karlos-hr <dev.carloshrios@gmail.com>
2024-08-13 02:19:17 +0200

    feat: nuevo project

--- src/app/data/mocks/projects.mock.ts
+++ src/app/data/mocks/projects.mock.ts
@@ -109,5 +109,81 @@ export const projects : IProject[] = [
         ]
 
     },
+    {
+        title: 'Shield Market',
+        subtitle: 'Market website',
+        tags : [
+            {
+                name:'Angular',
+                icon: 'skills/icons8-angular-48.png',
+                textColor: "text-white",
+                bgColor : "bg-red-900",
+                width: 14,
+                height: 14
+            },
+            {
+                name:'Django',
+                icon: 'django.icon.svg',
+                textColor: "text-white",
+                bgColor : "bg-green-900",
+                width: 14,
+                height: 14
+            },
+            {
+                name: 'HTML',
+                icon: 'html.icon.svg',
+                textColor: "text-white",
+                bgColor:"bg-orange-500",
+                width: 16,
+                height: 16
+            },
+            {
+                name: 'TypeScript',
+                icon: 'skills/icons8-typescript-48.png',
+                textColor: "text-white",
+                bgColor:"bg-blue-500",
+                width: 16,
+                height: 16
+            },
+            {
+                name: 'SCSS',
+                icon: 'css.icon.svg',
+                textColor: "text-white",
+                bgColor:"bg-pink-500",       
+                width: 16,
+                height: 16
+            },
+            {
+                name: 'Bootstrap',
+                icon: 'skills/icons8-bootstrap-48.png',
+                textColor: "text-white",
+                bgColor:"bg-purple-800",       
+                width: 16,
+                height: 16
+            },
+            {
+                name: 'Material',
+                icon: 'skills/material-design.svg',
+                textColor: "text-black",
+                bgColor:"bg-white",       
+                width: 16,
+                height: 16
+            },
+        ],
+        images: [
+            "projects/shield-market/1.png",
+            "projects/shield-market/2.png",
+            "projects/shield-market/3.png",
+            "projects/shield-market/4.png",
+            "projects/shield-market/5.png"
+        ],
+        link : "",
+        description: [
+            "Design and implement a scalable platform for selling and uploading plugins, supporting both free and premium offerings with various subscription plans.",
+            "Integrate user authentication features including email verification, secure sign-up processes, and role-based access control.",
+            "Implement a secure plugin upload system with built-in version control and mechanisms to protect the internal code of the plugins from unauthorized access or tampering."
+        ]
+
+    },
     
 ]
