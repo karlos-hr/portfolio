108209ac7c08351901452528322d92e8a17ab629
karlos-hr <dev.carloshrios@gmail.com>
2024-09-05 15:24:08 +0100

    refactor: 2 years

--- docs/chunk-LJECA6U6.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-DJYLR55W.js";import{A as e,L as m,a as i,c as r}from"./chunk-RAGHG5YT.js";var u=(()=>{let t=class t{};t.\u0275fac=function(n){return new(n||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[e,m]});let o=t;return o})();export{u as HomeModule};
