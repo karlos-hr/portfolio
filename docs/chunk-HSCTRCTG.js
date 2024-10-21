3160d258a162873bb2ffae55cbc7a4704caf7f29
karlos-hr <dev.carloshrios@gmail.com>
2024-10-21 17:00:15 +0100

    deploy

---
+++ docs/chunk-HSCTRCTG.js
@@ -0,0 +1,1 @@
+import"./chunk-E5LY6EXP.js";import{A as e,L as m,a as i,c as r}from"./chunk-JI46USEE.js";var u=(()=>{let t=class t{};t.\u0275fac=function(n){return new(n||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[e,m]});let o=t;return o})();export{u as HomeModule};
